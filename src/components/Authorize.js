0ea19c1dc81ae2fcb01263591b9d9d3bd0b2ee13
Erno Seppänen <ernseppa@gmail.com>
2021-03-08 16:18:27 +0200

    fiddling

--- src/components/Authorize.js
+++ src/components/Authorize.js
@@ -1,6 +1,6 @@
 
 const Authorize = () => {
-    const authUrl = 'https://api.instagram.com/oauth/authorize?client_id=168993578335569&redirect_uri=localhost:3000/authorize&scope=user_profile,user_media&response_type=code'
+    const authUrl = 'https://api.instagram.com/oauth/authorize?client_id=168993578335569&redirect_uri=https://ernoseppanen.com/&scope=user_profile,user_media&response_type=code'
 
     window.location.href = authUrl
     return (
--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 const PhotoList = () => {
-    console.log('photos to be added')
     return (
         <div>
             <p>PHOTOS TO BE ADDED</p>
