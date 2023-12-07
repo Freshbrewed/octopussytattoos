d14e56ed40a2052f8189ea2924e252a3b448d0dc
Erno Seppänen <ernseppa@gmail.com>
2023-12-07 22:24:43 +0200

    Rearranged Sneakpeek images

--- src/components/SneakPeek.js
+++ src/components/SneakPeek.js
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom'
 const SneakPeek = ({ media }) => {
     if (media === null) return <div>Loading . . .</div>
 
+
     const images = media.filter(element => element.media_type !== 'VIDEO')
+
 
     return (
         <div>
@@ -15,21 +17,21 @@ const SneakPeek = ({ media }) => {
             <div className='frontPictures'>
                 <div className='image-grid image-grid2'>
                     <img className='image imageSide' src={images[10].media_url} />
-                    <img className='image image2' src={images[9].media_url} />
-                    <img className='image image2' src={images[1].media_url} />
-                    <img className='image imageSide1' src={images[2].media_url} />
+                    <img className='image image2' src={images[0].media_url} />
+                    <img className='image image2' src={images[4].media_url} />
+                    <img className='image imageSide1' src={images[12].media_url} />
                 </div>
                 <div className='image-grid image-grid3'>
-                    <img className='image image2' src={images[3].media_url} />
-                    <img className='image image2' src={images[16].media_url} />
-                    <img className='image image2' src={images[6].media_url} />
-                    <img className='image image2' src={images[12].media_url} />
+                    <img className='image image2' src={images[8].media_url} />
+                    <img className='image image2' src={images[5].media_url} />
+                    <img className='image image2' src={images[15].media_url} />
+                    <img className='image image2' src={images[13].media_url} />
                 </div>
                 <div className='image-grid image-grid4'>
-                    <img className='image imageSide2' src={images[19].media_url} />
-                    <img className='image image2' src={images[4].media_url} />
-                    <img className='image image2' src={images[0].media_url} />
-                    <img className='image imageSide3' src={images[17].media_url} />
+                    <img className='image imageSide2' src={images[2].media_url} />
+                    <img className='image image2' src={images[9].media_url} />
+                    <img className='image image2' src={images[16].media_url} />
+                    <img className='image imageSide3' src={images[14].media_url} />
                 </div>
             </div>
         </div>
